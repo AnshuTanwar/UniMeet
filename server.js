617de73cff4afa71e9f5fd93c68b92656bbe5878
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-04-05 23:58:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Course = require('./models/course');
 
 const app = express();  
  
-app.engine('ejs', ejsMate);                  
+    app.engine('ejs', ejsMate);                  
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
