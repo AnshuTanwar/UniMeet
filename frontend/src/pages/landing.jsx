cc1be8bc7c2cb1151b80b9476be5be5d446a4b14
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-04-30 22:09:36 +0530

    New Commit

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function LandingPage() {
     return(
@@ -11,12 +12,18 @@ export default function LandingPage() {
                 <div className='navlist'>
                     <p>Join as Guest</p>
                     <p>Register</p>
-                    <div role='button'>Login</div>
+                    <div role='button'>Loginn</div>
                 </div>
             </nav>
 
             <div className="landingMainContainer">
-                <div></div>
+                <div>
+                    <h1><span style={{color: "#FF9839"}}>Connect </span>with your loved Ones</h1>
+                    <p>Meet anyone, anywhere, with <span style={{color: "#FF9839"}}>UniMeet</span></p>
+                    <div role='button'>
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
                 <div></div>
             </div>
         </div>
